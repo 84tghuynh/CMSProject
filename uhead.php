b471f8559aa7aead6bc1522519024c4494e815bd
84tghuynh <giangbrvt@gmail.com>
2019-11-25 23:57:09 -0600

    Implement search for not logged in users
    Implement usearch.php
    form: uhead.php

--- uhead.php
+++ uhead.php
@@ -1,6 +1,6 @@
 <form id="search" action="usearch.php" method="post">
   <div>
-     <input id="searchbox" type="text" />
+     <input id="searchbox" name="searchbox" type="text" />
      <input id="searchbutton" name="search" type="submit" value="Search" />
   </div>
 </form>
--- usearch.php
+++ usearch.php
@@ -1,9 +1,6 @@
 <?php
   require('connect.php');
-  session_start();
   $searchInput = '';
-  if(isset($_SESSION['email']))
-  {
       if
       (
         isset($_POST['search']) &&
@@ -15,16 +12,11 @@
           $searchInput = filter_input(INPUT_POST, 'searchbox', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
           // Query comment
           $query = 'SELECT * FROM products WHERE productName LIKE \'%'.$searchInput.'%\''.' OR '.'description LIKE  \'%'.$searchInput.'%\'';
-
           $stmt_product = $db->prepare($query);
           // $stmt_product->bindValue(':searchInput',$searchInput);
           $stmt_product->execute();
           $array_product = $stmt_product->fetchAll();
       }
-  }else{
-     header("Location: login.php");
-     exit();
-  }
 ?>
 <!DOCTYPE html>
 <html lang='en'>
@@ -34,22 +26,20 @@
     <link rel="stylesheet" href="style.css" type="text/css">
 </head>
 <body>
-    <?php include("head.php"); ?>
+    <?php include("uhead.php"); ?>
     <div id="header">
         <div id="header-left">
             <div><img src="img/ninja.png" alt="Florist"></div>
-            <div><a href="admin.php"><h1>CMS For Roy's Florist </h1></a></div>
-        </div>
-        <?php if($_SESSION['roletype']==1): ?>
-            <div id="header-right">
-                <div><a href="umanage.php">Manage User</a></div>
-            </div>
-        <?php endif ?>
-        <div id="header-right">
-            <div><a href="category.php">Category</a></div>
+            <div><a href="index.php"><h1>CMS For Roy's Florist </h1></a></div>
         </div>
         <div id="header-right">
-            <div><a href="create.php">New Product</a></div>
+            <div><a href="userregister.php">Register</a></div>
+        </div>
+        <div id="header-right">
+            <div><a href="login.php">Login</a></div>
+        </div>
+        <div id="header-right">
+            <div><a href="admin.php">Administrative</a></div>
         </div>
     </div>
     <div class="clear"></div>
@@ -69,8 +59,7 @@
             <?php if(count($array_product) >0 ): ?>
                 <?php foreach($array_product as $product): ?>
                         <div class="product_title">
-                            <h3><a href="show.php?id=<?= $product['productId'] ?>"><?= $product['productName'] ?></a></h3>
-                            <div class="edit_link"><a href="edit.php?id=<?= $product['productId'] ?>">edit</a></div>
+                            <h3><a href="ushow.php?id=<?= $product['productId'] ?>"><?= $product['productName'] ?></a></h3>
                             <div class="clear"></div>
                         </div>
                 <?php endforeach ?>
