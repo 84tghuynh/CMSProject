6de1a693d0ebaeb7ca5183aa5d5b930c61de1713
84tghuynh <giangbrvt@gmail.com>
2019-12-01 16:01:56 -0600

    Add category Search to head.php
    Add category Search to head.php for logged in users

--- head.php
+++ head.php
@@ -1,5 +1,22 @@
+<?php
+    require('connect.php');
+    $category_array = '';
+    $select = 'SELECT * FROM category ORDER BY created DESC';
+    // A PDO::Statement is prepared from the query.
+    $category = $db->prepare($select);
+    // Execution on the DB server is delayed until we execute().
+    $category->execute();
+    $category_array = $category->fetchAll();
+
+ ?>
 <form id="search" action="search.php" method="post">
   <div>
+    <select id="category" name="category">
+        <option value="">All</option>
+        <?php foreach($category_array as $cate): ?>
+          <option value="<?= $cate['categoryId'] ?>"><?= $cate['name']?></option>
+        <?php endforeach ?>
+    </select>
      <input id="searchbox" name="searchbox" type="text" />
      <input id="searchbutton" name="search" type="submit" value="Search" />
   </div>
